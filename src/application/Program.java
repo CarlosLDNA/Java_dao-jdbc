3a7df162517d14761292a1b0195090948200d9e7
CarlosLDNA <ca.gaspar@gmail.com>
2019-08-11 19:49:25 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.List;
+
 //import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-//import model.entities.Department;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,10 +17,16 @@ public class Program {
 		//Seller seller = new Seller(21, "Carlos", "carlos@teste.com", new Date(), 6000.0, department);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println("===== teste 1 FindById ======");
+		System.out.println("===== teste 1 Seller FindById ======");
 		Seller seller = sellerDao.findById(3);
-				
 		System.out.println(seller);
+		
+		System.out.println("\n===== teste 2 Seller FindByDepartment ======");		
+		Department department = new Department(2, null);	
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list ) {
+			System.out.println(obj);
+		}
 	}
 
 }
