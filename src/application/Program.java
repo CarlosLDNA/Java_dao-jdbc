89b282f4263ab287c437fd60b4f46476ffb19c93
CarlosLDNA <ca.gaspar@gmail.com>
2019-08-11 18:04:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Department department = new Department(1, "Livros");
+      
+		Seller seller = new Seller(21, "Carlos", "carlos@teste.com", new Date(), 6000.0, department);
+				
+		System.out.println(seller);
 	}
 
 }
